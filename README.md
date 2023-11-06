f39d433c54c291e21e2156a60d7a38ba42267767
Scezaquer <aurelien.buck.kaeffer@gmail.com>
2023-11-06 12:32:24 -0500

    switch to TOML

--- README.md
+++ README.md
@@ -2,10 +2,23 @@
 Core Library of PopRL
 
 # TODO: Release 1.0.0
-- [ ] Clean up main dir
+- [X] Clean up main dir
 - [ ] Divide features into Implemented and Future
 - [ ] Write documentation/example of phylogenetic tree.
-- [ ] Check requirements
-- [ ] Switch to TOML
-- [ ] Licence: MIT
+- [X] Check requirements
+- [X] Switch to TOML
+- [X] Licence: MIT
 - [ ] Upload to PyPi
+
+# Features
+
+### Implemented
+
+
+
+### Future
+
+
+
+# Documentation
+
--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 from distutils.core import setup
 
-setup(
-    name='popcore',
-    version='0.1',
-    description='Core',
-    author='Manfred Diaz, Aurelien Buck-Kaeffer',
-    author_email='',
-    url='https://www.python.org/sigs/distutils-sig/',
-    package_dir={'': 'src'},
-    packages=['popcore'],
-    extras_require={
-        'dev': [
-            'pytest',
-            'python-chess'
-        ]
-    }
-)
+setup()
+
+"""
+name='popcore',
+version='0.1',
+description='Core',
+author='Manfred Diaz, Aurelien Buck-Kaeffer',
+author_email='',
+url='https://www.python.org/sigs/distutils-sig/',
+package_dir={'': 'src'},
+packages=['popcore'],
+extras_require={
+    'dev': [
+        'pytest',
+        'python-chess'
+    ]
+}
+"""
